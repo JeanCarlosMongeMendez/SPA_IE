503917581fc8d50d7ef10970d7bc28164932950a
valerialeiva <Suisivale30>
2020-05-06 10:36:01 -0600

    Update HomeController.cs

--- SPA_IE/Controllers/HomeController.cs
+++ SPA_IE/Controllers/HomeController.cs
@@ -8,22 +8,9 @@ namespace SPA_IE.Controllers
 {
     public class HomeController : Controller
     {
-<<<<<<< HEAD
-        private readonly ILogger<HomeController> _logger;
 
-        /// <summary>
-        /// Prueba
-        /// </summary>
-        /// <param name="logger"></param>
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-=======
+   
         public ActionResult Index()
->>>>>>> 88c736a2f0e438f231fa3a9f746db20bae9cc5c7
         {
             return View();
         }
