50de8b6965e46aa8eda935f497b4ca43ecee9877
lyka-guillen-a <62961193+lyka-guillen-a@users.noreply.github.com>
2020-05-19 03:06:51 -0600

    Test

---
+++ UnitTestSPA_IE/CrudStudentTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPA_IE.Models.Data.Data;
+using SPA_IE.Models.Data.DTO;
+using SPA_IE.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTestSPA_IE
+{
+    /// <summary>
+    /// Descripción resumida de CrudStudent
+    /// </summary>
+    [TestClass]
+    public class CrudStudentTest
+    {
+        [TestMethod]
+        public void InsertStudentCorrectly()
+        {
+            //Arrange
+            StudentController studentController = new StudentController();
+            StudentDTO student = new StudentDTO();
+            student.IdStudent= 1000;
+            student.IdentificationCard = "B79038";
+            student.isASIP = false;
+            student.isActive = true;
+
+            //Act
+            ActionResult result = studentController.Create(student) as ActionResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+
+        }
+    }
+}
