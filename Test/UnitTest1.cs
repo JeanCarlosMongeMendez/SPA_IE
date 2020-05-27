3dd8155ea3afe67e54a7b02d83261a8cc1799b84
lyka-guillen-a <62961193+lyka-guillen-a@users.noreply.github.com>
2020-05-27 15:58:21 -0600

    Two test

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -9,7 +9,14 @@ namespace Test
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1,2);
+            Assert.AreEqual(1,1);
+        }
+    
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 2);
         }
     }
 }
