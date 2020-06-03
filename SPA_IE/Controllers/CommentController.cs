46f82bf6056eddcb64f0b3435232e14b981d1c08
JeanCarlosMongeMendez <42584455+JeanCarlosMongeMendez@users.noreply.github.com>
2020-06-03 00:46:05 -0600

    Revert "Merge branch 'Branch_CI' into Branch_Lyka"
    This reverts commit a1f151a6dd587cf1d8035adbbba282effd069ce0, reversing
    changes made to d8af9129134a2873bf9e6035a93fce9ceb23c109.

--- SPA_IE/Controllers/CommentController.cs
+++ SPA_IE/Controllers/CommentController.cs
@@ -1,9 +1,10 @@
-﻿using SPA_IE.Models.Data;
-using SPA_IE.Models.Data.Data;
+﻿using Newtonsoft.Json;
 using SPA_IE.Models.Domain.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,89 +13,170 @@ namespace SPA_IE.Controllers
     public class CommentController : Controller
     {
         // GET: Comment
-        CommentData commentData = new CommentData();
-        public PartialViewResult GetAll()
+        public ActionResult Index()
         {
-            try
+            return View();
+        }
+        public IEnumerable<CommentDTO> commentsList()
+        {
+            IEnumerable<CommentDTO> comments = null;
+            using (var client = new HttpClient())
             {
-                IEnumerable<SelectComment_Result> comments = commentData.ListAllSP();
-            return PartialView("GetAll", comments);
-            
+                client.BaseAddress = new Uri("https://localhost:44357/api/Comment/");
+                try
+                {
+                    var responseTask = client.GetAsync("GetAllCommentSP");
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<IList<CommentDTO>>();
+                        readTask.Wait();
+                        comments = readTask.Result;
+                    }
+                    else
+                    {
+                        comments = Enumerable.Empty<CommentDTO>();
+                        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator");
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+
             }
-            catch
-            {
-                throw;
-            }
+            return comments;
+
         }
 
 
-        public PartialViewResult Create()
+        public PartialViewResult List()
         {
-            try
-            {
-                return PartialView("Create");
-            }
-            catch
-            {
-                throw;
-            }
+
+            return PartialView("List", commentsList());
         }
 
-        [HttpPost]
-        public PartialViewResult Create(CommentDTO comment)
-        {
-            try
-            {
-
-                commentData.Add(comment);
-            return PartialView("GetAll", commentData.ListAllSP().AsEnumerable());
-            }
-            catch
-            {
-                throw;
-            }
-        }
 
         public PartialViewResult Edit(int id)
         {
-            try
+            CommentDTO commentDTO = null;
+            using (var client = new HttpClient())
             {
-                
-            CommentDTO comment = commentData.GetById(id);
-
-            return PartialView(comment);
+                client.BaseAddress = new Uri("https://localhost:44357/api/Comment/");
+                try
+                {
+                    var responseTask = client.GetAsync("GetComment/" + id);
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<CommentDTO>();
+                        readTask.Wait();
+                        commentDTO = readTask.Result;
+                    }
+                    else
+                    {
+                        commentDTO = new CommentDTO();
+                        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator");
+                    }
+                }
+                catch (AggregateException agg_ex)
+                {
+                    var ex = agg_ex.InnerExceptions[0];
+                }
             }
-            catch
-            {
-                throw;
-            }
+            return PartialView("Edit", commentDTO);
         }
 
         [HttpPost]
         public PartialViewResult Edit(CommentDTO comment)
         {
-            try
+            int apiResult = 0;
+            using (var client = new HttpClient())
             {
-                commentData.Update(comment);
-            return PartialView("GetAll", commentData.ListAllSP().AsEnumerable());
+                client.BaseAddress = new Uri("https://localhost:44357/api/Comment/");
+                try
+                {
+                    var json = JsonConvert.SerializeObject(comment);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    var responseTask = client.PutAsync("PutComment", content);
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<Int32>();
+                        readTask.Wait();
+                        apiResult = readTask.Result;
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
             }
-            catch
-            {
-                throw;
-            }
+            return PartialView("List", commentsList());
         }
 
+        public PartialViewResult Create()
+        {
+
+            return PartialView("Create");
+        }
+
+        [HttpPost]
+        public PartialViewResult Create(CommentDTO comment)
+        {
+            int apiResult = 0;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44357/api/Comment/");
+                try
+                {
+                    var json = JsonConvert.SerializeObject(comment);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    var responseTask = client.PostAsync("PostComment", content);
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<Int32>();
+                        readTask.Wait();
+                        apiResult = readTask.Result;
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+            return PartialView("List", commentsList());
+        }
         public PartialViewResult Delete(int id)
         {
-            try
+            int apiResult = 0;
+            using (var client = new HttpClient())
             {
-                commentData.Delete(id);
-            return PartialView("GetAll", commentData.ListAllSP().AsEnumerable());
+                client.BaseAddress = new Uri("https://localhost:44357/api/Comment/");
+                try
+                {
+                    var responseTask = client.DeleteAsync("DeleteComment/" + id);
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<Int32>();
+                        readTask.Wait();
+                        apiResult = readTask.Result;
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
             }
-            catch
-            {
-                throw;
-            }
+            return PartialView("List", commentsList());
         }
+   
     }
 }
